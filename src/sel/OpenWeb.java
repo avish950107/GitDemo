bf866aaffb79bed527eb7bea339e857dde6f6097
avish950107 <avish950107@gmail.com>
2021-01-08 14:06:15 +0530

    this is commit from asian time zone guy

--- src/sel/OpenWeb.java
+++ src/sel/OpenWeb.java
@@ -10,8 +10,12 @@ public class OpenWeb {
 		System.setProperty("webdriver.chrome.driver","F://Softwares//chromedriver_win32//chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
 
+		System.out.println(driver.getTitle());
 		driver.get("https://www.google.co.in/");
 		
+		System.out.println(driver.getCurrentUrl());
+		
+		System.out.println("Script run successfully by person Y");
 		driver.close();
 	}
 
